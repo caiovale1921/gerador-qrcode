dbde2d5ed23d6d02ec35a56f09098aea6cb8f7ea
Caio <caio1@LAPTOP-EE98JVOS>
2021-10-02 19:18:54 -0300

    Criação da classe para controle dos dados passados para a geração do qrcode e organanização do codigo.

---
+++ GeradorQRCode/Models/QrCodeClass.cs
@@ -0,0 +1,36 @@
+﻿using QRCoder;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GeradorQRCode.Models
+{
+    public class QrCodeClass
+    {
+        public Byte[] QrCodeImg { get; set; }
+
+        public string QrText { get; set; }
+
+        public QrCodeClass(string qrText)
+        {
+            QrText = qrText;
+            QRCodeGenerator qrCodeGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrCodeGenerator.CreateQrCode(QrText, QRCodeGenerator.ECCLevel.Q);
+            QRCode qRCode = new QRCode(qrCodeData);
+            var qrCodeImg = qRCode.GetGraphic(20);
+            QrCodeImg = BitmapToBytes(qrCodeImg);
+        }
+
+        private static Byte[] BitmapToBytes(Bitmap img)
+        {
+            using (MemoryStream stream = new MemoryStream())
+            {
+                img.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+                return stream.ToArray();
+            }
+        }
+    }
+}
